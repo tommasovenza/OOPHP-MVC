ee98762d8d85533443154406c92349edb6924d76
Tommaso <tommasovenza@gmail.com>
2026-04-10 15:27:56 +0200

    created getUrl method for testing

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,9 +1,21 @@
 <?php
 
-class Core
-{
+/**
+ * Creates URL & loads core controller
+ * URL FORMAT - /controller/method/params
+*/
+class Core {
+    protected $currentController = 'Public';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
     public function __construct()
     {
-        echo 'Hello from Core class!';
+        echo 'Hello from Core class!' . "<br>";
+        $this->getUrl();
+    }
+
+    public function getUrl() {
+        echo $_GET['url'] ?? 'url params NOT found!';
     }
 }
